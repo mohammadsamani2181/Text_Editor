ffe7da972b4631920740bb4e3a42e57ac1d03739
mmd_s21 <hdhdmohammad5@gmail.com>
2022-12-05 18:15:04 +0330

    created and createNewPage method added

---
+++ src/com/company/PageNode.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class PageNode {
+    private LinesLinkedList info; // contains all lines in this page
+    private PageNode next; // next page (node) after this page in linkedList
+
+    public PageNode(ArrayList<String> page) {
+        info = createNewPage(page);
+        next = null;
+    }
+
+    public PageNode getNext() {
+        return next;
+    }
+
+    public void setNext(PageNode next) {
+        this.next = next;
+    }
+
+    // creates a new LinesLinkedList and adds all lines of new page to it
+    public LinesLinkedList createNewPage(ArrayList<String> page) {
+        LinesLinkedList newPage = new LinesLinkedList();
+
+        for (int i = 0; i < page.size(); i++) {
+            newPage.addLine(page.get(i));
+        }
+
+        return newPage;
+    }
+}
