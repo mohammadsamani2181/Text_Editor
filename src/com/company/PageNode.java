f44bf5f3be17cf0af2c3e72414b03a8ce4d89a2a
mmd_s21 <hdhdmohammad5@gmail.com>
2022-12-08 14:44:04 +0330

    append, insert, remove, replace, swap, find, findAndReplace methods added

--- src/com/company/PageNode.java
+++ src/com/company/PageNode.java
@@ -43,4 +43,42 @@ public class PageNode {
     public void print(int n) {
         info.print(n);
     }
+
+    // appends the given string to the end of the page (node)
+    public void append(String[] strings) {
+        for (String line : strings) {
+            info.addLine(line);
+        }
+    }
+
+    // inserts the given string to the nth line of the page (node)
+    public void insert(String newLine, int n) {
+        info.insert(newLine, n);
+    }
+
+    // removes the nth line of the page
+    public void remove(int n) {
+        info.remove(n);
+    }
+
+    // replaces the nth line of the page with the given string "newLine"
+    public void replace(int n, String newLine) {
+        info.replace(n, newLine);
+    }
+
+    // swaps line n with line m in the linkedList
+    public void swap(int n, int m) {
+        info.swap(n, m);
+    }
+
+    // search for a specific string in the page (node)
+    public String find(String string) {
+        return info.find(string);
+    }
+
+    // finds all usage of string "oldString" in the page (node)
+    // and replace them with the string "newString"
+    public void findAndReplace(String oldString, String newString) {
+        info.findAndReplace(oldString, newString);
+    }
 }
